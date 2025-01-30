4468aa0b13235a33c4d52cbe90baeaff52957861
chantie84 <cc.crompvoets@gmail.com>
2025-01-30 13:59:30 +0100

    Injectected more weather data

--- src/app.js
+++ src/app.js
@@ -2,11 +2,39 @@ function updateWeather(response) {
   let temperatureElement = document.querySelector("#temperature");
   let temperature = response.data.temperature.current;
   let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windSpeedElement = document.querySelector("#wind-speed");
+  let timeElememt = document.querySelector("#time");
+  let date = new Date(response.data.time * 1000);
 
   cityElement.innerHTML = response.data.city;
+
+  timeElememt.innerHTML = formatDate(date);
+  descriptionElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+  windSpeedElement.innerHTML = `${response.data.wind.speed} km/h`;
   temperatureElement.innerHTML = Math.round(temperature);
 }
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Teusday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saterday",
+  ];
+  let day = days[date.getDay()];
 
+  if (minutes < 10) {
+    mintues = `0${minutes}`;
+  }
+  return `${day} ${hours}:${minutes}`;
+}
 function searchCity(city) {
   let apiKey = "e1e10606109dc39aa96a4o5b4facfd3t";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
